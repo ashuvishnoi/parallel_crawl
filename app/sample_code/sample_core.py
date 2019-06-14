bda4606155aac15f4e8756dd7024437515902f9d
Ashutosh <ashutosh.vishnoi.790@gmail.com>
2019-06-14 16:07:11 +0530

    parallel_crawl

--- app/sample_code/sample_core.py
+++ app/sample_code/sample_core.py
@@ -1,17 +1,6 @@
-from newsplease import NewsPlease
-import numpy as np
-
 from configuration import num_url
-
 import logging
-logger = logging.getLogger(__name__)
-
 from newsplease import NewsPlease
-import numpy as np
-
-# from configuration import num_url
-
-import logging
 
 logger = logging.getLogger(__name__)
 
@@ -20,6 +9,8 @@ def get_url_batches(urls):
     batches = [urls[i:i + num_url] for i in range(0, len(urls), num_url)]
     return batches
 
+def get_dict(crawlled_data):
+    return data_crawled=[{i: j.get_dict()} for obj in crawlled_data for i, j in obj.items()]
 
 def parallel_crawl(urls):
     url_batches = get_url_batches(urls)
@@ -30,7 +21,7 @@ def parallel_crawl(urls):
         data = NewsPlease.from_urls(batch)
         crawlled_data.append(data)
 
-    data_crawled=[{i: j.get_dict()} for obj in crawlled_data for i, j in obj.items()]
+    data_crawled = get_dict(crawlled_data)
     logger.info(f"get response {data_crawled}")
 
     return data_crawled
